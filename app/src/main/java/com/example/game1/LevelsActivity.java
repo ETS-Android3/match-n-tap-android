7b358c6946e821137825f6317e9c3a5d6a69db3a
cs13b1043 <cs13b1043@iith.ac.in>
2018-02-15 20:23:07 +0530

    created LevelsActivity

---
+++ app/src/main/java/com/example/game1/LevelsActivity.java
@@ -0,0 +1,20 @@
+package com.example.game1;
+
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class LevelsActivity extends AppCompatActivity {
+
+    private static final String TAG = "LevelsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_levels);
+    }
+}
